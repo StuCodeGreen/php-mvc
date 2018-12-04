750c47454f7f3d33925406c7fcc6fe798e685178
Martynas Kuznecovas <m.kuznecovas@gmail.com>
2018-12-04 17:38:47 +0000

    users controler

---
+++ app/controllers/Users.php
@@ -0,0 +1,28 @@
+<?php
+class Users extends Controller {
+    public function __construct() {
+
+    }
+
+    public function register() {
+
+        // check for post
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //process form
+        } else {
+            // init data
+            $data = [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm_password' => '',
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => '',
+            ];
+        }
+        //load view
+        $this->view('users/register',$data);
+    }
+}
