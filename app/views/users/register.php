248d803b4de1640af17eaf1fbbddd706841c783b
Martynas Kuznecovas <m.kuznecovas@gmail.com>
2018-12-05 17:59:35 +0000

    login form

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -34,7 +34,7 @@
               <input type="submit" value="Register" class="btn btn-success btn-block">       
             </div>
             <div class="col">
-              <a href="<?php URLROOT; ?>/users/login" class="btn btn-light btn-block"> Have an account ? Login</a>
+              <a href="<?php echo URLROOT?>/users/login" class="btn btn-light btn-block"> Have an account ? Login</a>
             </div>
           </div>
         </form>
