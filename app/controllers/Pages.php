1c8cc1011b33bd0e81b0e6e9eff36ee3246cbccd
stucodegreen <m.kuznecovas@gmail.com>
2019-01-03 17:40:58 +0000

    add post form

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,9 @@ class Pages extends Controller {
     }
 
     public function index() {
+				if(isLoggedIn()){
+					redirect('posts');
+				}
         $data = [
             'title' => 'Share Posts',
             'description' => 'Sit tempor et magna ipsum diam vero ea duo, ut.',
