9097c2748c99312b96a3b5f73fcb530132d99467
stucodegreen <m.kuznecovas@gmail.com>
2019-01-11 17:14:54 +0000

    update

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -19,6 +19,9 @@
         <ul class="navbar-nav ml-auto">
 				<?php if(isset($_SESSION['user_id'])) : ?>
 				<li class="nav-item ">
+            <a class="nav-link">Welcome <?php echo $_SESSION['user_name']; ?> </a>
+          </li>
+				<li class="nav-item ">
             <a class="nav-link" href="<?php echo URLROOT;?>/users/logout">Logout </a>
           </li>
 				<?php else : ?>
